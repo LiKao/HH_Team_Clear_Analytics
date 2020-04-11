a2ee2502e64bd65c7b1e0a4eb03cc3d6bfda8dbc
Tillmann Nett <mail@tcrueger.de>
2020-04-11 21:59:52 +0200

    Imputed missing values as zero and reversed color scale

--- 30_GIS_NYC/Heatmap_NYC.R
+++ 30_GIS_NYC/Heatmap_NYC.R
@@ -2,11 +2,11 @@ rm(list = ls())
 
 library(tidyverse)
 library(sf)
-dists <- st_read("bezirksgrenzen.shp")
+dists <- st_read("ZIP_CODE_040114.shp")
 
-df.cases <- read.csv2("bezirkstabelle.csv", stringsAsFactors = F) %>%
-  mutate(Bezirk = gsub("\xf6","ö",Bezirk))
+df.cases <- read.csv("tests-by-zcta.csv", stringsAsFactors = F) %>%
+  mutate(MODZCTA = as.character(MODZCTA))
 
-dists <- dists %>% left_join(df.cases, by = c("Gemeinde_n" = "Bezirk"))
+dists <- dists %>% left_join(df.cases, by = c("ZIPCODE" = "MODZCTA"))
 
-ggplot(dists, aes(fill=Fallzahl)) + geom_sf() + scale_fill_gradientn(colors=c("red","yellow","white"))
+ggplot(dists, aes(fill=Positive)) + geom_sf() + scale_fill_gradientn(colors=c("red","yellow","white"))
