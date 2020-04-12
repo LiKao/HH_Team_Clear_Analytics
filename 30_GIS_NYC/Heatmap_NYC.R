964a417389ddb867ce84cdfd42156512a205b5c8
Tillmann Nett <mail@tcrueger.de>
2020-04-12 18:36:41 +0200

    Minor changes missed in last commit

--- 30_GIS_NYC/Heatmap_NYC.R
+++ 30_GIS_NYC/Heatmap_NYC.R
@@ -67,13 +67,24 @@ ggmap(NYCmap) +
 
 # Detailed view of the path
 
-NYCmap.path = get_googlemap( c(lon = -74, lat = 40.75), zoom = 11,
+NYCmap.path = get_googlemap( c(lon = -74, lat = 40.75), zoom = 12,
                              style = c(feature = "all", element = "labels", visibility = "off"),
                              maptype = "satellite")
 ggmap(NYCmap.path)
 
 ggmap(NYCmap.path) +
   geom_polygon(data=df.cases.rastered, aes(long, lat, group=group, fill=Positive, alpha=alpha)) +
+  scale_fill_gradientn(colors=c("yellow","red")) +
+  scale_alpha_continuous(range=c(0,0.7)) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  scale_x_continuous(name="",breaks = NULL) +
+  scale_y_continuous(name="",breaks = NULL) +
+  coord_equal(xlim=c(-74.1,-73.9), ylim=c(40.68,40.82))
+
+ggmap(NYCmap.path) +
+  geom_polygon(data=df.cases.rastered, aes(long, lat, group=group, fill=Positive, alpha=alpha)) +
+  geom_path(data=path, aes(long, lat), color="blue", size=1.5) +
   scale_fill_gradientn(colors=c("yellow","red")) +
   scale_alpha_continuous(range=c(0,0.7)) +
   theme_minimal() +
