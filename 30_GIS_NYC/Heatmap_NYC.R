7e72a8e6eb3023bcf4fe68f87ff2bc0e6697c2ff
Tillmann Nett <mail@tcrueger.de>
2020-04-12 17:56:43 +0200

    Overlay with map data from googlemaps and path in New York

--- 30_GIS_NYC/Heatmap_NYC.R
+++ 30_GIS_NYC/Heatmap_NYC.R
@@ -2,6 +2,9 @@ rm(list = ls())
 
 library(tidyverse)
 library(sf)
+library(ggmap)
+library(rgdal)
+
 dists <- st_read("ZIP_CODE_040114.shp")
 
 df.cases <- read.csv("tests-by-zcta.csv", stringsAsFactors = F) %>%
@@ -9,4 +12,72 @@ df.cases <- read.csv("tests-by-zcta.csv", stringsAsFactors = F) %>%
 
 dists <- dists %>% left_join(df.cases, by = c("ZIPCODE" = "MODZCTA"))
 
-ggplot(dists, aes(fill=Positive)) + geom_sf() + scale_fill_gradientn(colors=c("red","yellow","white"))
+dists$Positive <- ifelse(is.na(dists$Positive),0,dists$Positive)
+
+ggplot(dists, aes(fill=Positive)) + geom_sf() + scale_fill_gradientn(colors=c("white","yellow","red"))
+
+
+NYCmap = get_googlemap( c(lon = -73.95, lat = 40.7), zoom = 10,
+                        style = c(feature = "all", element = "labels", visibility = "off"),
+                        maptype = "satellite")
+ggmap(NYCmap)
+
+NYCZips <- readOGR("./NYCZips/") %>% spTransform(CRS("+proj=longlat +datum=WGS84"))
+ggplot(data = NYCZips, aes(x = long, y = lat, group = group)) + geom_path()
+
+# convert to a format in which case data can be merged in
+
+# Rasters the shape data into a single dataframe and joins case data
+df.cases.rastered  <- NYCZips %>%
+  fortify(region = "ZIPCODE") %>%
+  left_join(NYCZips@data, by = c("id" = "ZIPCODE")) %>%
+  left_join(df.cases, by=c("id" = "MODZCTA"))
+
+cases.max <- max(df.cases.rastered$Positive, na.rm = T)
+df.cases.rastered$alpha <- ifelse(is.na(df.cases.rastered$Positive),0,df.cases.rastered$Positive/cases.max+0.3)
+
+
+ggplot(df.cases, aes(long, lat, group=group, fill=Positive)) +
+  geom_polygon() +
+  scale_fill_gradientn(colors=c("white","yellow","red"))
+
+
+ggmap(NYCmap) +
+  geom_polygon(data=df.cases.rastered, aes(long, lat, group=group, fill=Positive, alpha=alpha)) +
+  scale_fill_gradientn(colors=c("yellow","red")) +
+  scale_alpha_continuous(range=c(0,0.7)) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  scale_x_continuous(name="",breaks = NULL) +
+  scale_y_continuous(name="",breaks = NULL)
+
+# retrieve the movement path
+path <- readOGR("./path/","doc-line") %>% spTransform(CRS("+proj=longlat +datum=WGS84"))
+
+ggmap(NYCmap) +
+  geom_polygon(data=df.cases.rastered, aes(long, lat, group=group, fill=Positive, alpha=alpha)) +
+  geom_path(data=path, aes(long, lat), color="blue", width=3) +
+  scale_fill_gradientn(colors=c("yellow","red")) +
+  scale_alpha_continuous(range=c(0,0.7)) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  scale_x_continuous(name="",breaks = NULL) +
+  scale_y_continuous(name="",breaks = NULL)
+
+
+# Detailed view of the path
+
+NYCmap.path = get_googlemap( c(lon = -74, lat = 40.75), zoom = 11,
+                             style = c(feature = "all", element = "labels", visibility = "off"),
+                             maptype = "satellite")
+ggmap(NYCmap.path)
+
+ggmap(NYCmap.path) +
+  geom_polygon(data=df.cases.rastered, aes(long, lat, group=group, fill=Positive, alpha=alpha)) +
+  scale_fill_gradientn(colors=c("yellow","red")) +
+  scale_alpha_continuous(range=c(0,0.7)) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  scale_x_continuous(name="",breaks = NULL) +
+  scale_y_continuous(name="",breaks = NULL) +
+  coord_equal(xlim=c(-74.1,-73.9), ylim=c(40.68,40.82))
